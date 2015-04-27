4c85ecbde69e3f273e8644a692646a809a5922db
jpozoal <josedlpozo@gmail.com>
2015-04-27 23:14:06 +0200

    creacion author y footer

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controllers');
-
+var author = require('../controllers/author');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
 router.get('/quizes/question', quizController.question);
+
 router.get('/quizes/answer', quizController.answer);
+
+router.get('/author' , author.author);
 
 module.exports = router;
