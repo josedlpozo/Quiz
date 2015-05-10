ea374e53eaf418d49464fbead5adbd83d66e50f7
jpozoal <josedlpozo@gmail.com>
2015-05-10 19:32:36 +0200

    login y autorizacion

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,23 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controllers');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
+
 var author = require('../controllers/author');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
 
+// Definición de rutas de quizes
 router.param('quizId', quizController.load);
+
+// Definición de rutas de sesion
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
+
+
 
 router.get('/quizes', quizController.index);
 
@@ -17,18 +27,19 @@ router.get('/quizes/:quizId(\\d+)', quizController.show);
 
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
-router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
 
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
 
-router.get('/quizes/new', quizController.new);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
 
-router.post('/quizes/create', quizController.create);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
 
 router.get('/author' , author.author);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
